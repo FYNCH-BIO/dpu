1820ee83191d4e0e2ad9b89cfa26f4625b5d1883
cpmancuso <cmancuso@bu.edu>
2019-04-29 15:01:20 -0400

    Code Cleanup

--- experiment/template/main_eVOLVER.py
+++ experiment/template/main_eVOLVER.py
@@ -11,8 +11,7 @@ import smtplib
 class make_GUI:
 
     def __init__(self, master):
-
-        master.wm_title("eVOLVER Test")
+        master.wm_title(custom_script.GUI_NAME)
         note = Notebook(master)
         home = Frame(note)
         note.add(home, text = "Home")
@@ -25,10 +24,10 @@ class make_GUI:
             note.add(newTab, text = "{0}".format(x))
             tabArray.append(newTab)
 
-        self.quitButton = Button(home, text="Stop Measuring", command=stop_exp)
+        self.quitButton = Button(home, text="Pause Measuring / Stop Pumps", command=stop_exp)
         self.quitButton.pack(side=BOTTOM)
 
-        self.printButton = Button(home, text="Start/ Measure now", command=start_exp)
+        self.printButton = Button(home, text="Start / Measure now", command=start_exp)
         self.printButton.pack(side=BOTTOM)
 
         note.pack(fill=BOTH, expand=YES)
@@ -58,7 +57,7 @@ def update_eVOLVER():
     ##Read and record OD
     elapsed_time = round((time.time() - start_time)/3600,4)
     print("Time: {0} Hours".format(elapsed_time))
-    OD_data, temp_data = eVOLVER_module.read_data(vials, exp_name)
+    OD_data, temp_data = eVOLVER_module.read_data(vials)
     skip = False
     if OD_data is not None and temp_data is not None:
         if OD_data == 'empty':
@@ -66,33 +65,35 @@ def update_eVOLVER():
         else:
             for x in vials:
                 OD_data[x] = OD_data[x] - OD_initial[x]
-        eVOLVER_module.parse_data(OD_data, elapsed_time,vials,exp_name, 'OD')
-        eVOLVER_module.parse_data(temp_data, elapsed_time,vials,exp_name, 'temp')
+        eVOLVER_module.parse_data(OD_data, elapsed_time, vials, 'OD')
+        eVOLVER_module.parse_data(temp_data, elapsed_time, vials, 'temp')
 
-        ##Make decision
-        custom_functions(elapsed_time,exp_name)
+        #Make decision
+        custom_functions(elapsed_time)
 
         #Save Variables
         global run_exp
-        eVOLVER_module.save_var(exp_name, start_time, OD_initial)
+        eVOLVER_module.save_var(start_time, OD_initial)
     run_exp = root.after(1000,update_eVOLVER)
 
 
 #### Custom Script
-def custom_functions(elapsed_time, exp_name):
+def custom_functions(elapsed_time):
     global OD_data, temp_data
     if OD_data == 'empty':
         print("UDP Empty, did not execute program!")
     else:
         ###load script from another python file
-        custom_script.test(OD_data, temp_data, vials, elapsed_time, exp_name)
+        if custom_script.OPERATION_MODE == 'turbidostat':
+            custom_script.turbidostat(OD_data, temp_data, vials, elapsed_time)
+        if custom_script.OPERATION_MODE == 'chemostat':
+            custom_script.chemostat(OD_data, temp_data, vials, elapsed_time)
 
 
 #### Runs if this is main script
 if __name__ == '__main__':
-    exp_name, evolver_ip, evolver_port = custom_script.choose_name()
     vials = range(0,16)
-    start_time, OD_initial  =  eVOLVER_module.initialize_exp(exp_name, vials, evolver_ip, evolver_port)
+    start_time, OD_initial = eVOLVER_module.initialize_exp(vials)
     root=Tk()
     make_GUI(root)
     update_eVOLVER()
