98444d98c8f041ab872d79a938cb68fc008203f6
Zachary Heins <zackheins@gmail.com>
2019-06-26 10:35:32 -0400

    3D calibrations and flattened json processing

--- calibration/calibrate.py
+++ calibration/calibrate.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit, leastsq
 from socketIO_client import SocketIO, BaseNamespace
 from scipy.linalg import lstsq
+import scipy, scipy.optimize
 import asyncio
 from threading import Thread
 import json
@@ -60,36 +61,39 @@ class EvolverNamespace(BaseNamespace):
             print(f)
         cal_received = True
 
+def SurfacePlot(func, data, fittedParameters, i, fig):
+    x_data = data[0]
+    y_data = data[1]
+    z_data = data[2]
+
+    xModel = np.linspace(min(x_data), max(x_data), 20)
+    yModel = np.linspace(min(y_data), max(y_data), 20)
+    X, Y = np.meshgrid(xModel, yModel)
+
+    Z = func(np.array([X, Y]), *fittedParameters)
+
+    ax = fig.add_subplot(4,4, i + 1, projection = '3d')
+
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=1, antialiased=True, alpha=0.5)
+
+    ax.scatter(x_data, y_data, z_data, c='r', s=10) # show data along with plotted surface
+
+    ax.set_title('Surface Plot (click-drag with mouse)') # add a title for surface plot
+    ax.set_xlabel('X Data') # X axis data label
+    ax.set_ylabel('Y Data') # Y axis data label
+    ax.set_zlabel('Z Data') # Z axis data label
+
 def parse_od_cal_data(data):
-    global input_data, odaxis
+    cal_ods = {'od90': [0] * 16, 'od135': [0] * 16, 'temp':[0] * 16}
+    vial_datas = data['vialData']
 
-    input_data = data['inputData'] #load data from json
-
-    yaxis_levels = []
-    for cal_measurement in data['vialData']:
-        yaxis_levels.insert(len(yaxis_levels),cal_measurement[odaxis]) # identify power levels from json
-
-    yaxis_levels = list(set(yaxis_levels)) # drop replicated values
-    cal_ods = {}
-    for level in yaxis_levels:
-        cal_ods[level] = [] #create json to house data for each level
-
-    for cal_measurement in data['vialData']:
-        power_level = cal_measurement[odaxis]
-        for i,od in enumerate(cal_measurement['od']):
-            if len(cal_ods[power_level]) == i:
-                cal_ods[power_level].append([])
-            cal_ods[power_level][i].append(od)
-
-    for power_level, vial_datas in cal_ods.items():
-        for i, vial_data in enumerate(vial_datas):
-            new_vial_data = [0] * 16
-            # Shift indexes to handle rotation of vials
+    for param, param_vial_datas in vial_datas.items():
+        for i, vial_data in enumerate(param_vial_datas):
+            cal_ods[param][i] = [0] * 16
             for j in range(16):
+                # Shift indexes to handle rotation of vials
                 index_change = -j + i
-                new_vial_data[j] = vial_data[index_change]
-            cal_ods[power_level][i] = new_vial_data
-
+                cal_ods[param][i][j] = vial_data[index_change]
     return cal_ods
 
 def parse_temp_cal_data(data):
@@ -103,6 +107,11 @@ def linear(x, a, b):
     y = np.array(x)*a + b
     return y
 
+def three_dim(data, c0,c1,c2,c3,c4,c5,c6,c7,c8,c9):
+    x = data[0]
+    y = data[1]
+    return c0 + c1*x + c2*y + c3*x**2 + c4*x*y + c5*y**2 + c6*x**3 + c7*(x**2)*y + c8*x*(y**2) + c9*(y**3)
+
 def fit(x, y):
     #Fits the data to a sigmoid and returns the parameters
     xdata = np.array(x)
@@ -112,17 +121,6 @@ def fit(x, y):
     paramlin, pcovlin = curve_fit(linear, xdata, ydata, p0 = [-8750, 62721])
     return paramsig, paramlin;
 
-def fit3d(x, y, z):
-    x = np.array(x)
-    y = np.array(y)
-    z = np.array(z)
-
-    data = np.transpose(np.stack((x, y, z), axis=0))
-    # best-fit quadratic curve: Z = C[4]*X**2. + C[5]*Y**2. + C[3]*X*Y + C[1]*X + C[2]*Y + C[0]`
-    A = np.c_[np.ones(data.shape[0]), data[:,:2], np.prod(data[:,:2], axis=1), data[:,:2]**2]
-    paramquad,_,_,_ = lstsq(A, data[:,2])
-    return paramquad, data;
-
 def residuals(curve_y, real_y):
     #Calculates residuals using the least square method
     curve_arr = np.array(curve_y)
@@ -131,39 +129,30 @@ def residuals(curve_y, real_y):
     sumsquare = res.sum()
     return sumsquare
 
-def calibrate_od(calibration_data, graph_name, graph= False):
+def calibrate_od(calibration_data, graph_name, param, graph = False):
     global input_data
-    power_levels = []
-    if odpower == 'all':
-        power_levels = sorted(np.fromiter(calibration_data.keys(), dtype=int), reverse = False)
-    else:
-        power_levels = [int(odpower)]
-
     paramlist = []
     if graph:
         fig1, ax = plt.subplots(4, 4)
         fig1.suptitle('File Name: ' + graph_name)
 
-    for level in power_levels:
-        for m in range(16):
-            raw_ods = []
-            stds = []
-            for raw_od in calibration_data[level][m]:
-                raw_ods.append(np.mean(raw_od))
-                stds.append(np.std(raw_od))
+    for m in range(16):
+        raw_ods = []
+        stds = []
+        for raw_od in calibration_data[param][m]:
+            raw_ods.append(np.median(raw_od))
+            stds.append(np.std(raw_od))
 
-            paramsig, paramlin = fit(input_data,raw_ods)
-            paramlist.append(np.array(paramsig).tolist())
-            if graph == True:
-                fit_label = str(odaxis) + ': ' + str(level) if m == 0 else None
-                ax[m//4, (m % 4)].plot(input_data, raw_ods, 'o', markersize=1.5, color='black')
-                ax[m//4, (m % 4)].plot(np.linspace(0, max(input_data), 500), sigmoid(np.linspace(0, max(input_data), 500), *paramsig), markersize = 1.5, label=fit_label)
-                ax[m//4, (m % 4)].errorbar(input_data, raw_ods, yerr=stds, fmt='none')
-                ax[m//4, (m % 4)].set_title('Vial: ' + str(m))
-                ax[m//4, (m % 4)].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        paramsig, paramlin = fit(input_data,raw_ods)
+        paramlist.append(np.array(paramsig).tolist())
+        if graph == True:
+            ax[m//4, (m % 4)].plot(input_data, raw_ods, 'o', markersize=1.5, color='black')
+            ax[m//4, (m % 4)].plot(np.linspace(0, max(input_data), 500), sigmoid(np.linspace(0, max(input_data), 500), *paramsig), markersize = 1.5, label=None)
+            ax[m//4, (m % 4)].errorbar(input_data, raw_ods, yerr=stds, fmt='none')
+            ax[m//4, (m % 4)].set_title('Vial: ' + str(m))
+            ax[m//4, (m % 4)].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
     if graph:
-        fig1.legend(loc = 'lower center', ncol=len(power_levels))
         plt.subplots_adjust(hspace = 0.6)
         plt.show()
     return paramlist
@@ -171,44 +160,41 @@ def calibrate_od(calibration_data, graph_name, graph= False):
 
 def calibrate3d_od(calibration_data, graph_name, graph= False):
     global input_data
-    power_levels = sorted(np.fromiter(raw_calibration.keys(), dtype=int), reverse = False)
 
-    paramlist = []
-    if graph:
-        fig = plt.figure()
-        fig.suptitle('File Name: ' + graph_name)
-
-    for m in range(16): ##vial
-        x= []; y = []; z = []
-        for input_index, od_value in enumerate(input_data):
-            for power_index, level in enumerate(power_levels):
-                x.append(np.mean(calibration_data[level][m][input_index]))
-                y.append(level)
-                z.append(od_value)
-
-
-        paramquad, data = fit3d(x, y, z)
-        paramlist.append(np.array(paramquad).tolist())
-
-        xs = np.arange(np.amin(x), np.amax(x),1000)
-        xy = np.arange(np.amin(y), np.amax(y),100)
-        (X, Y) = np.meshgrid(xs,xy)
-        XX = X.flatten()
-        YY = Y.flatten()
-
-        Z = np.dot(np.c_[np.ones(XX.shape), XX, YY, XX*YY, XX**2, YY**2], paramquad).reshape(X.shape) # evaluate it on a grid
-
-        if graph == True:
-            ax = fig.add_subplot(4, 4, m+1, projection='3d')
-            ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.2)
-            ax.scatter(data[:,0], data[:,1], data[:,2], c='r', s=50)
-            ax.set_title('Vial: ' + str(m))
+    initial_parameters = [1.0, 1.0, 1.0,1.0,1.0,1.0, 1.0, 1.0,1.0,1.0]
 
     if graph:
-        plt.subplots_adjust(hspace = 0.6)
+        fig1 = plt.figure()
+        fig1.suptitle('File Name: ' + graph_name)
+
+    for i in range(16):
+        x_data = np.median(calibration_data['od90'][i], axis = 1)
+        y_data = np.median(calibration_data['od135'][i], axis = 1)
+        z_data = np.array(input_data)
+
+        data = [x_data, y_data, z_data]
+
+        fitted_parameters, pcov = scipy.optimize.curve_fit(three_dim, [x_data, y_data], z_data, p0 = initial_parameters)
+        if graph:
+            SurfacePlot(three_dim, data, fitted_parameters, i, fig1)
+
+        modelPredictions = three_dim(data, *fitted_parameters)
+
+        absError = modelPredictions - z_data
+
+        SE = np.square(absError) # squared errors
+        MSE = np.mean(SE) # mean squared errors
+        RMSE = np.sqrt(MSE) # Root Mean Squared Error, RMSE
+        Rsquared = 1.0 - (np.var(absError) / np.var(z_data))
+        print('Vial ' + str(i))
+        print('RMSE:', RMSE)
+        print('R-squared:', Rsquared)
+        print('fitted prameters', fitted_parameters)
+
+    if graph:
         plt.show()
 
-    return paramlist
+    return fitted_parameters
 
 def calibrate_temp(calibration_data, graph_name, graph = False):
     paramlist = []
@@ -266,6 +252,7 @@ if __name__ == '__main__':
     parser.add_option('--odpower', action = 'store', dest = 'odpower', default='2500')
     parser.add_option('--odaxis', action = 'store', dest = 'odaxis', default='powerLevel')
     parser.add_option('--3d', action = 'store_true', dest = 'multidimfit')
+    parser.add_option('--param', action = 'store', dest = 'param')
 
 
     (options, args) = parser.parse_args()
@@ -277,11 +264,15 @@ if __name__ == '__main__':
     odpower = options.odpower
     odaxis = options.odaxis
     multidimfit = options.multidimfit
+    param = options.param
 
     if not options.ipaddress:
         print('Please specify ip address')
         parser.print_help()
-        sys.exit(1)
+        sys.exit(2)
+
+    if not param:
+        param = 'od90'
 
     new_loop = asyncio.new_event_loop()
     t = Thread(target = start_background_loop, args = (new_loop,))
@@ -313,7 +304,7 @@ if __name__ == '__main__':
         pass
 
     if cal_file is not None and od_cal and not multidimfit:
-        parameters = calibrate_od(raw_calibration, cal_file, graph = True)
+        parameters = calibrate_od(raw_calibration, cal_file, param, graph = True)
         update_cal = input('Update eVOLVER with calibration? (y/n): ')
         if update_cal == 'y':
             dpu_evolver_ns.emit('setcalibrationod', {'parameters':parameters, 'filename': cal_file, 'caltype': 'sigmoid'}, namespace='/dpu-evolver')
@@ -321,7 +312,7 @@ if __name__ == '__main__':
         parameters = calibrate3d_od(raw_calibration, cal_file, graph = True)
         update_cal = input('Update eVOLVER with calibration? (y/n): ')
         if update_cal == 'y':
-            dpu_evolver_ns.emit('setcalibrationod', {'parameters':parameters, 'filename': cal_file, 'caltype': 'multidim_quad'}, namespace='/dpu-evolver')
+            dpu_evolver_ns.emit('setcalibrationod', {'parameters':parameters, 'filename': cal_file, 'caltype': 'multidim'}, namespace='/dpu-evolver')
     if cal_file is not None and temp_cal:
         parameters = calibrate_temp(raw_calibration, cal_file, graph = True)
         update_cal = input('Update eVOLVER with calibration? (y/n): ')
