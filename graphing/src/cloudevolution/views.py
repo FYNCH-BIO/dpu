fd4aa328ff56bfd8a9c42508ee5fe6b62fd9bb48
Daniel García Ruano <dani14_96@usal.es>
2020-05-04 14:18:48 +0200

    Add media consumption tracking (#20)
    * Add dilution calculation to graphing tool
    
    Calculate dilution using pump_log and pump_cal.
    
    Add table html template and update navbar links
    and url patterns.
    
    * Add dilution efficiency
    
    Calculate and display table of percentages of dilution
    efficiency. Display time of last dilution.
    
    * Add requested changes
    
    - Use os.path.join()
    - Change navbar button name for dilutions
    
    * Remove pump efficiency table for chemostats
    
    When efficiency is 0 for all vials, table is
    removed and a notice is displayed.
    
    Co-authored-by: Daniel García Ruano <daniel.garciaruano@univ-rennes1.fr>

--- graphing/src/cloudevolution/views.py
+++ graphing/src/cloudevolution/views.py
@@ -33,9 +33,9 @@ def vial_num(request, experiment, vial):
 	vial_count = range(0, 16)
 	expt_dir, expt_subdir = file_scan(experiment)
 	rootdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-	evolver_dir = rootdir + '/experiment'
-	OD_dir = evolver_dir + "/%s/%s/OD/vial%s_OD.txt" % (expt_subdir[0],experiment,vial)
-	temp_dir = evolver_dir + "/%s/%s/temp/vial%s_temp.txt" % (expt_subdir[0],experiment,vial)
+	evolver_dir = os.path.join(rootdir, 'experiment')
+	OD_dir = os.path.join(evolver_dir, expt_subdir[0], experiment, "OD", "vial{0}_OD.txt".format(vial))
+	temp_dir = os.path.join(evolver_dir, expt_subdir[0], experiment, "temp", "vial{0}_temp.txt".format(vial))
 
 
 	with open(OD_dir) as f_in:
@@ -94,25 +94,79 @@ def expt_name(request, experiment):
 
 	return render(request, "experiment.html", context)
 
+def dilutions(request, experiment):
+	sidebar_links, subdir_log = file_scan('expt')
+	vial_count = range(0, 16)
+	expt_dir, expt_subdir = file_scan(experiment)
+	rootdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+	evolver_dir = os.path.join(rootdir, 'experiment')
+	pump_cal = os.path.join(evolver_dir, expt_subdir[0], "pump_cal.txt")
 
+	cal = np.genfromtxt(pump_cal, delimiter="\t")
+	diluted = []
+	efficiency = []
+	last = []
+
+	for vial in vial_count:
+		pump_dir = os.path.join(evolver_dir, expt_subdir[0], experiment, "pump_log", "vial{0}_pump_log.txt".format(vial))
+		ODset_dir = os.path.join(evolver_dir, expt_subdir[0], experiment, "ODset", "vial{0}_ODset.txt".format(vial))
+		data = np.genfromtxt(pump_dir, delimiter=',', skip_header=2)
+
+		dil_triggered = len(data)
+
+		if dil_triggered != 0:
+			volume = str(round(sum(data[:, 1]) * cal[0, vial] / 1000, 2))
+
+			dil_intervals = len(np.genfromtxt(ODset_dir, delimiter=",", skip_header=2)) / 2
+			if dil_intervals != 0:
+				extra_dils = dil_triggered - dil_intervals
+				vial_eff = (dil_intervals - extra_dils) / dil_intervals * 100
+			else:
+				# Experiment is chemostat or vial is not used
+				vial_eff = 0
+
+		else:
+			volume = 0
+			vial_eff = 0
+
+		diluted.append(volume)
+		efficiency.append(str(round(vial_eff, 1)))
+		last.append(time.ctime(os.path.getmtime(pump_dir)))
+
+	last_dilution = max(last)
+
+	if efficiency == ['0']*16:
+		# All vials were chemostats or not used
+		efficiency = None
+
+	context = {
+	"sidebar_links": sidebar_links,
+	"experiment": experiment,
+	"vial_count": vial_count,
+	"diluted": diluted,
+	"efficiency": efficiency,
+	"last_dilution": last_dilution
+	}
+
+	return render(request, "dilutions.html", context)
 
 
 def file_scan(tag):
 	rootdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-	evolver_dir = rootdir + '/experiment'
+	evolver_dir = os.path.join(rootdir, "experiment")
 	url_string = '{%s url "home" %s}' % ('%','%')
 
 	sidebar_links =[]
 	subdir_log = []
 
 	for subdir in next(os.walk(evolver_dir))[1]:
-	    subdirname = os.path.join(next(os.walk(evolver_dir))[0], subdir)
+		subdirname = os.path.join(next(os.walk(evolver_dir))[0], subdir)
 
-	    for subsubdir in next(os.walk(subdirname))[1]:
-	        if tag in subsubdir:
-	            #add_string = "<li><a href='http://127.0.0.1:8000/%s'>%s</a></li>" % (subsubdir,subsubdir)
-	            #add_string = "<li><a href='%s'>%s</a></li>" % (url_string,subsubdir)
-	            sidebar_links.append(subsubdir)
-	            subdir_log.append(subdir)
+		for subsubdir in next(os.walk(subdirname))[1]:
+			if tag in subsubdir:
+				#add_string = "<li><a href='%s'>%s</a></li>" % (url_string,subsubdir)
+				sidebar_links.append(subsubdir)
+				subdir_log.append(subdir)
+				subdir_log.append(subdir)
 
 	return sidebar_links,subdir_log
