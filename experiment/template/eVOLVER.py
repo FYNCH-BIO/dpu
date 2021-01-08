db8f81972408aeb1e196e017957c838cb45214db
Zachary Heins <zackheins@gmail.com>
2021-01-08 09:57:19 -0500

    Adding features for electron usage

--- experiment/template/eVOLVER.py
+++ experiment/template/eVOLVER.py
@@ -12,6 +12,7 @@ import json
 import traceback
 from scipy import stats
 from socketIO_client import SocketIO, BaseNamespace
+from nbstreamreader import NonBlockingStreamReader as NBSR
 
 import custom_script
 from custom_script import EXP_NAME, PUMP_CAL_FILE
@@ -24,9 +25,10 @@ from custom_script import STIR_INITIAL, TEMP_INITIAL
 VIALS = [x for x in range(16)]
 
 SAVE_PATH = os.path.dirname(os.path.realpath(__file__))
-EXP_DIR = os.path.join(SAVE_PATH, EXP_NAME)
+EXP_DIR = os.path.join(SAVE_PATH, 'data')
 OD_CAL_PATH = os.path.join(SAVE_PATH, 'od_cal.json')
 TEMP_CAL_PATH = os.path.join(SAVE_PATH, 'temp_cal.json')
+JSON_PARAMS_FILE = os.path.join(SAVE_PATH, 'eVOLVER_parameters.json')
 
 SIGMOID = 'sigmoid'
 LINEAR = 'linear'
@@ -34,12 +36,15 @@ THREE_DIMENSION = '3d'
 
 logger = logging.getLogger('eVOLVER')
 
+paused = False
+
 EVOLVER_NS = None
 
 class EvolverNamespace(BaseNamespace):
     start_time = None
     use_blank = False
     OD_initial = None
+    experiment_params = None
 
     def on_connect(self, *args):
         print("Connected to eVOLVER as client")
@@ -306,7 +311,8 @@ class EvolverNamespace(BaseNamespace):
             text_file.write(default + '\n')
         text_file.close()
 
-    def initialize_exp(self, vials, always_yes=False):
+    def initialize_exp(self, vials, experiment_params, always_yes = False):
+        self.experiment_params = experiment_params
         logger.debug('initializing experiment')
 
         if os.path.exists(EXP_DIR):
@@ -382,7 +388,13 @@ class EvolverNamespace(BaseNamespace):
                                             "0,0,0"],
                                   directory='chemo_config')
 
-            self.update_stir_rate(STIR_INITIAL)
+            stir_rate = STIR_INITIAL
+            temp_values = TEMP_INITIAL
+            if self.experiment_params:
+                stir_rate = list(map(lambda x: x['stir'], self.experiment_params['vial_configuration']))
+                temp_values = list(map(lambda x: x['temp'], self.experiment_params['vial_configuration']))
+            self.update_stir_rate(stir_rate)
+            self.update_temperature(temp_values)
 
             if always_yes:
                 exp_blank = 'y'
@@ -409,7 +421,7 @@ class EvolverNamespace(BaseNamespace):
         # copy current custom script to txt file
         backup_filename = '{0}_{1}.txt'.format(EXP_NAME,
                                             time.strftime('%y%m%d_%H%M'))
-        shutil.copy('custom_script.py', os.path.join(EXP_DIR,
+        shutil.copy(os.path.join(SAVE_PATH, 'custom_script.py'), os.path.join(EXP_DIR,
                                                     backup_filename))
         logger.info('saved a copy of current custom_script.py as %s' %
                     backup_filename)
@@ -533,23 +545,24 @@ class EvolverNamespace(BaseNamespace):
 
     def custom_functions(self, data, vials, elapsed_time):
         # load user script from custom_script.py
-        if OPERATION_MODE == 'turbidostat':
+        mode = self.experiment_params['function'] if self.experiment_params else OPERATION_MODE
+        if mode == 'turbidostat':
             custom_script.turbidostat(self, data, vials, elapsed_time)
-        elif OPERATION_MODE == 'chemostat':
+        elif mode == 'chemostat':
             custom_script.chemostat(self, data, vials, elapsed_time)
         else:
             # try to load the user function
             # if failing report to user
-            logger.info('user-defined operation mode %s' % OPERATION_MODE)
+            logger.info('user-defined operation mode %s' % mode)
             try:
-                func = getattr(custom_script, OPERATION_MODE)
+                func = getattr(custom_script, mode)
                 func(self, data, vials, elapsed_time)
             except AttributeError:
                 logger.error('could not find function %s in custom_script.py' %
-                            OPERATION_MODE)
+                            mode)
                 print('Could not find function %s in custom_script.py '
                     '- Skipping user defined functions'%
-                    OPERATION_MODE)
+                    mode)
 
     def stop_exp(self):
         self.stop_all_pumps()
@@ -576,7 +589,6 @@ def get_options():
     log_nolog.add_argument('--quiet', action='store_true',
                            default=False,
                            help='Disable logging to file entirely')
-
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -588,14 +600,26 @@ if __name__ == '__main__':
     # silence logging until experiment is initialized
     logging.level = logging.CRITICAL + 10
 
-    socketIO = SocketIO(EVOLVER_IP, EVOLVER_PORT)
+    experiment_params = None
+    if os.path.exists(JSON_PARAMS_FILE):
+        with open(JSON_PARAMS_FILE) as f:
+            experiment_params = json.load(f)
+    evolver_ip = experiment_params['ip'] if experiment_params is not None else EVOLVER_IP
+    socketIO = SocketIO(evolver_ip, EVOLVER_PORT)
     EVOLVER_NS = socketIO.define(EvolverNamespace, '/dpu-evolver')
 
     # start by stopping any existing chemostat
     EVOLVER_NS.stop_all_pumps()
     #
     EVOLVER_NS.start_time = EVOLVER_NS.initialize_exp(VIALS,
-                                                      options.always_yes)
+                                                      experiment_params,
+                                                      options.always_yes,
+                                                      )
+
+    # Using a non-blocking stream reader to be able to listen
+    # for commands from the electron app. 
+    nbsr = NBSR(sys.stdin)
+    paused = False
 
     # logging setup
     if options.quiet:
@@ -612,19 +636,35 @@ if __name__ == '__main__':
                             level=level)
 
     reset_connection_timer = time.time()
-    while True:
+    while True:        
         try:
             # infinite loop
-            socketIO.wait(seconds=0.1)
-            if time.time() - reset_connection_timer > 3600:
-                # reset connection to avoid buildup of broadcast
-                # messages (unlikely but could happen for very long
-                # experiments with slow dpu code/computer)
-                logger.info('resetting connection to eVOLVER to avoid '
-                            'potential buildup of broadcast messages')
+
+            # check if a message has come in from the DPU
+            message = nbsr.readline()
+            if 'pause-script' in message:
+                print('Pausing experiment', flush = True)
+                logger.info('Pausing experiment in dpu')
+                paused = True
+                EVOLVER_NS.stop_exp()
                 socketIO.disconnect()
+                
+            if 'continue-script' in message:
+                print('Restarting experiment', flush = True)
+                paused = False
                 socketIO.connect()
-                reset_connection_timer = time.time()
+
+            if not paused:
+                    socketIO.wait(seconds=0.1)
+                    if time.time() - reset_connection_timer > 3600 and not paused:
+                        # reset connection to avoid buildup of broadcast
+                        # messages (unlikely but could happen for very long
+                        # experiments with slow dpu code/computer)
+                        logger.info('resetting connection to eVOLVER to avoid '
+                                    'potential buildup of broadcast messages')
+                        socketIO.disconnect()
+                        socketIO.connect()
+                        reset_connection_timer = time.time()
         except KeyboardInterrupt:
             try:
                 print('Ctrl-C detected, pausing experiment')
